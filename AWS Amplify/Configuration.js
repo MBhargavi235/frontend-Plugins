2e11f7b1f8f80c000a1030cc7476ddcc21239c8a
MBhargavi235 <106804698+MBhargavi235@users.noreply.github.com>
2023-02-09 16:09:27 +0530

    Update Configuration.js

--- AWS Amplify/Configuration.js
+++ AWS Amplify/Configuration.js
@@ -1,3 +1,8 @@
+
+// Create USerpool in AWS 
+// Goto Userpool Overview (User pool ID) and App Integration tab (go to App clients and analytics use Client ID as userPoolWebClientId )
+
+
 const awsExports = {
     Auth: {
         'userPoolId':'ap-south-1_vbDzGNxKm',
